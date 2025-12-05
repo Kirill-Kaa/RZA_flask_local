a5b42aaa50e53c6ce0ec7e2d20d8ef45aa3550b5
Kirill-Kaa <kirillkaa11@gmail.com>
2025-12-05 10:30:18 +0000

    v0.5

--- app.py
+++ app.py
@@ -110,6 +110,54 @@ def hotel():
 
     return render_template("hotel_booking.html")
 
+def init_db():
+   conn = get_db()
+   c = conn.cursor()
+   # Users table
+   c.execute("""
+   CREATE TABLE IF NOT EXISTS users(
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       username TEXT UNIQUE NOT NULL,
+       password TEXT NOT NULL
+   )
+   """)
+
+   # Ticket types table
+   c.execute("""
+   CREATE TABLE IF NOT EXISTS ticket_types(
+       ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+       type TEXT NOT NULL,
+       cost INTEGER NOT NULL
+   )
+   """)
+
+   # Bookings table
+   c.execute("""
+   CREATE TABLE IF NOT EXISTS bookings(
+       booking_id INTEGER PRIMARY KEY AUTOINCREMENT,
+       user_id INTEGER NOT NULL,
+       ticket_id INTEGER NOT NULL,
+       people INTEGER NOT NULL,
+       date TEXT NOT NULL,
+       FOREIGN KEY (user_id) REFERENCES users(id),
+       FOREIGN KEY (ticket_id) REFERENCES ticket_types(ticket_id)
+   )
+   """)
+   # hotelbooking table
+   c.execute("""
+   CREATE TABLE IF NOT EXISTS hotelbooking (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       user_id INTEGER,
+       check_in TEXT,
+       check_out TEXT,
+       rooms INTEGER,
+       adults INTEGER,
+       children INTEGER
+   )
+   """)
+
+   conn.commit()
+   conn.close()
 
 @app.route("/my-bookings")
 def my_bookings():
--- test_app.py
+++ test_app.py
@@ -1,12 +1,23 @@
 import unittest
-from app import app  # import your Flask instance
-
+from app import app, init_db  # import your Flask instance
+import os
 class TestPageLoad(unittest.TestCase):
 
     def setUp(self):
         # Create a test client
         self.client = app.test_client()
         app.config['TESTING'] = True
+        app.config["SECRET_KEY"] = "test"
+        app.config["DATABASE"] = "test.db"
+     # 2. Delete old DB BEFORE anything opens it
+        if os.path.exists("test.db"):
+            os.remove("test.db")
+
+        # 3. Now build fresh schema
+        init_db()
+
+        # 4. Create test client
+        self.client = app.test_client()
 
     def test_register_page_loads(self):
         response = self.client.get('/register')
